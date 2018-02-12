e2a02b8537965c81c5026d0abc5f2be889c96547
wc23 <wc23@indiana.edu>
2018-02-12 13:08:00 -0500

    graph generator

--- simulation.py
+++ simulation.py
@@ -1,8 +1,9 @@
 
+import math
 from models import DeffuantModel
-import networkx  # networkx 2.x
+import networkx as nx  # networkx 2.x
 import os
-
+import pickle
 class Node(object):
 
   def __init__(self, opinion):
@@ -15,9 +16,10 @@ class OpinionSim(object):
     self.graph = None
     self.num_nodes = num_nodes
     self.num_edges = num_edges
+    self.params = kwargs
     if model == 'deffuant':
       self.model = DeffuantModel(self.graph, kwargs)
-
+    self.generateGraph(kwargs['structure'])
     self.directory = directory
     os.mkdir(self.directory)
     os.mkdir(os.path.join(self.directory, 'opinion_data'))
@@ -36,16 +38,22 @@ class OpinionSim(object):
     ''' initialize self.graph
     '''
     if network_structure == 'lattice2D':
-      pass
+      m = math.sqrt(self.num_nodes)
+      self.graph = nx.grid_2d_graph(m, m)
+      self.num_edges = self.graph.num_of_edges()
     elif network_structure == 'scaleFree':
-      pass
+      assert self.num_edges
+      self.graph = nx.barabasi_albert_graph(self.num_nodes, self.num_edges)
     elif network_structure == 'smallWorld':
       pass
     elif network_structure == 'randomNetwork':
       assert self.num_edges
-      pass
+      self.graph = nx.gnm_random_graph(self.num_nodes, self.num_edges)
     elif network_structure == 'twoCommunities':
-      pass
+      nx.newman_watts_strogatz_graph(self.num_nodes,
+                                     self.kwargs['k'],
+                                     self.kwargs['p'])
+      self.num_edges = self.graph.num_of_edges()
     else:
       print('network is not initialized...')
       raise
@@ -63,4 +71,8 @@ class OpinionSim(object):
 
   def outputGraph(self, iteration):
     '''output graph with opinions'''
-    pass
+    nx.write_gexf(self.graph, '%s/network_data/%s' % (self.directory,
+                                                      iteration))
+    opinion_dict = nx.get_node_attributes(self.graph, 'opinion')
+    with open("%s/opinion_data/%s.pkl" % (self.directory, iteration), 'wb') as pickleFile:
+      pickle.dump(opinion_dict, pickleFile)
