a642910bf4f9342434ca12f7ab0b354dea470b43
Wen Chen <wc23@indiana.edu>
2018-02-13 22:29:28 -0500

    final vs init opinion plot

--- analyzer.py
+++ analyzer.py
@@ -32,18 +32,34 @@ class Analyzer(object):
     plt.hist(opinions, range=(0, 1))
     plt.savefig('%s/analysis/opinionDist%s.png' % (self.folder, c_time))
 
-  def networkVisualization(self, iteration):
+  def networkVisualization(self, c_time):
+    # Gephi is better for this visualization
     pass
+
+  def finalVSInitOpinion(self):
+    plt.clf()
+    plt.axis([0,1,0,1])
+    init_time = self.time[0]
+    final_time = self.time[-1]
+    with open('%s/opinion_data/%s' % (self.folder, init_time), "rb") as fd:
+      init_opinions = pickle.load(fd)
+    with open('%s/opinion_data/%s' % (self.folder, final_time), "rb") as fd:
+      final_opinions = pickle.load(fd)
+    plt.plot(init_opinions,final_opinions,'o')
+    plt.xlabel('i')
+    plt.ylabel('f')
+    plt.savefig('%s/analysis/final_vs_init.png' % (self.folder))
 
   def TopoInfluence(self):
     """influence to segregation(distance, opinion)"""
+
     pass
 
   def ParamInfluence(self, param):
     """influence to segregation(distance, opinion)"""
     pass
 
-folder = 'Data_2018021322031120'
+folder = 'Data_2018021322260930'
 files = os.listdir('%s/opinion_data' % folder)
 times = []
 for f in files:
@@ -54,5 +70,6 @@ for f in files:
 times.sort()
 
 analyzor = Analyzer(folder, times)
-analyzor.opinionEvolve()
+#analyzor.opinionEvolve()
 #analyzor.opinionDistribution(times[-1])
+analyzor.finalVSInitOpinion()
